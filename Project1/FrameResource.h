3c6a490ea59e7f961c7927f974161b798942829a
david <eorjs2020@gmail.com>
2021-03-22 15:40:05 -0400

    Metrial need to fix

--- Project1/FrameResource.h
+++ Project1/FrameResource.h
@@ -8,6 +8,9 @@ struct ObjectConstants
 {
     DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
 	DirectX::XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+    DirectX::XMFLOAT2 DisplacementMapTexelSize = { 1.0f, 1.0f };
+    float GridSpatialStep = 1.0f;
+    float Pad;
 };
 
 struct PassConstants
@@ -29,6 +32,11 @@ struct PassConstants
 
     DirectX::XMFLOAT4 AmbientLight = { 0.0f, 0.0f, 0.0f, 1.0f };
 
+
+    DirectX::XMFLOAT4 FogColor = { 0.7f, 0.7f, 0.7f, 1.0f };
+    float gFogStart = 5.0f;
+    float gFogRange = 150.0f;
+    DirectX::XMFLOAT2 cbPerObjectPad2;
     // Indices [0, NUM_DIR_LIGHTS) are directional lights;
     // indices [NUM_DIR_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHTS) are point lights;
     // indices [NUM_DIR_LIGHTS+NUM_POINT_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHT+NUM_SPOT_LIGHTS)
@@ -49,6 +57,7 @@ struct FrameResource
 {
 public:
     
+    FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount, UINT waveVertCount);
     FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount);
     FrameResource(const FrameResource& rhs) = delete;
     FrameResource& operator=(const FrameResource& rhs) = delete;
@@ -65,6 +74,8 @@ public:
     std::unique_ptr<UploadBuffer<MaterialConstants>> MaterialCB = nullptr;
     std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
 
+
+    std::unique_ptr<UploadBuffer<Vertex>> WavesVB = nullptr;
     // Fence value to mark commands up to this fence point.  This lets us
     // check if these frame resources are still in use by the GPU.
     UINT64 Fence = 0;
--- Project1/Shaders/TreeSprite.hlsl
+++ Project1/Shaders/TreeSprite.hlsl
@@ -1,7 +1,5 @@
 //***************************************************************************************
-// Default.hlsl 
-//
-// Default shader, currently supports lighting.
+// TreeSprite.hlsl.
 //***************************************************************************************
 
 // Defaults for number of lights.
@@ -19,8 +17,11 @@
 
 // Include structures and functions for lighting.
 #include "LightingUtil.hlsl"
+//step5
+Texture2DArray gTreeMapArray : register(t0);
 
-Texture2D    gDiffuseMap : register(t0);
+//you can use dynamic indexing as well. Pay attention how we changed the sampler!
+//Texture2D gTreeMapArray[3] : register(t0);
 
 
 SamplerState gsamPointWrap        : register(s0);
@@ -75,46 +76,106 @@ cbuffer cbMaterial : register(b2)
     float    gRoughness;
 	float4x4 gMatTransform;
 };
-
+ 
 struct VertexIn
 {
-	float3 PosL    : POSITION;
-    float3 NormalL : NORMAL;
-	float2 TexC    : TEXCOORD;
+	float3 PosW  : POSITION;
+	float2 SizeW : SIZE;
 };
 
 struct VertexOut
 {
+	float3 CenterW : POSITION;
+	float2 SizeW   : SIZE;
+};
+
+struct GeoOut
+{
 	float4 PosH    : SV_POSITION;
     float3 PosW    : POSITION;
     float3 NormalW : NORMAL;
-	float2 TexC    : TEXCOORD;
+    float2 TexC    : TEXCOORD;
+    uint   PrimID  : SV_PrimitiveID;
 };
 
 VertexOut VS(VertexIn vin)
 {
-	VertexOut vout = (VertexOut)0.0f;
+	VertexOut vout;
+
+	// Just pass data over to geometry shader.
+	vout.CenterW = vin.PosW;
+	vout.SizeW   = vin.SizeW;
+
+	return vout;
+}
+ 
+//step4
+ // We expand each point into a quad (4 vertices), so the maximum number of vertices
+ // we output per geometry shader invocation is 4.
+[maxvertexcount(4)]
+void GS(point VertexOut gin[1], //PrimitiveType InputVertexType InputName[NumElements]-> one vertex for a point, two for aline, threefor atriangle, four for line with adjacency, and six for a triangle with adjacency.
+        uint primID : SV_PrimitiveID, //inout StreamOutputObject<OutputVertexType>
+        inout TriangleStream<GeoOut> triStream)  //OutputName
+{	
+	//
+	// Compute the local coordinate system of the sprite relative to the world
+	// space such that the billboard is aligned with the y-axis and faces the eye.
+	//
+
+	float3 up = float3(0.0f, 1.0f, 0.0f);
+	float3 look = gEyePosW - gin[0].CenterW;
+	look.y = 0.0f; // y-axis aligned, so project to xz-plane
+	look = normalize(look);
+	float3 right = cross(up, look);
+
+	//
+	// Compute triangle strip vertices (quad) in world space.
+	//
+	float halfWidth  = 0.5f*gin[0].SizeW.x;
+	float halfHeight = 0.5f*gin[0].SizeW.y;
 	
-    // Transform to world space.
-    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
-    vout.PosW = posW.xyz;
+	float4 v[4];
+	v[0] = float4(gin[0].CenterW + halfWidth*right - halfHeight*up, 1.0f);
+	v[1] = float4(gin[0].CenterW + halfWidth*right + halfHeight*up, 1.0f);
+	v[2] = float4(gin[0].CenterW - halfWidth*right - halfHeight*up, 1.0f);
+	v[3] = float4(gin[0].CenterW - halfWidth*right + halfHeight*up, 1.0f);
 
-    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
-    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
-
-    // Transform to homogeneous clip space.
-    vout.PosH = mul(posW, gViewProj);
+	//
+	// Transform quad vertices to world space and output 
+	// them as a triangle strip.
+	//
 	
-	// Output vertex attributes for interpolation across triangle.
-	float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
-	vout.TexC = mul(texC, gMatTransform).xy;
-
-    return vout;
+	float2 texC[4] = 
+	{
+		float2(0.0f, 1.0f),
+		float2(0.0f, 0.0f),
+		float2(1.0f, 1.0f),
+		float2(1.0f, 0.0f)
+	};
+	
+	GeoOut gout;
+	[unroll]
+	for(int i = 0; i < 4; ++i)
+	{
+		gout.PosH     = mul(v[i], gViewProj);
+		gout.PosW     = v[i].xyz;
+		gout.NormalW  = look;
+		gout.TexC     = texC[i];
+		gout.PrimID   = primID;
+		
+		triStream.Append(gout);
+	}
 }
 
-float4 PS(VertexOut pin) : SV_Target
+//step6
+float4 PS(GeoOut pin) : SV_Target
 {
-    float4 diffuseAlbedo = gDiffuseMap.Sample(gsamAnisotropicWrap, pin.TexC) * gDiffuseAlbedo;
+	float3 uvw = float3(pin.TexC, pin.PrimID%3);
+    float4 diffuseAlbedo = gTreeMapArray.Sample(gsamAnisotropicWrap, uvw) * gDiffuseAlbedo;
+
+    //using dynamic indexing
+    //float4 diffuseAlbedo = gTreeMapArray[pin.PrimID % 3].Sample(gsamAnisotropicWrap, pin.TexC) * gDiffuseAlbedo;
+
 	
 #ifdef ALPHA_TEST
 	// Discard pixel if texture alpha < 0.1.  We do this test as soon 
